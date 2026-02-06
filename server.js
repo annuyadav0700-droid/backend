bbe5dfee759c1c83c42f48d93ad117b5dc427cc3
Annu <annuyadav0700@gmail.com>
2026-02-06 21:33:59 +0530

    fixed verification crash

--- server.js
+++ server.js
@@ -64,10 +64,11 @@ app.post("/create-order", async (req, res) => {
 //PAYMENT VERIFICATION
 app.post("/verify-payment",(req, res) =>
 {
+  const crypto = require("crypto");
   try{
     const {razorpay_order_id, razorpay_payment_id, razorpay_signature}
     = req.body;
-    const body = razorpay_order_id + "|"
+    const body = razorpay_order_id + "|" +
      razorpay_payment_id;
     const expectedSignature = crypto
       .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
@@ -80,7 +81,7 @@ app.post("/verify-payment",(req, res) =>
       res.status(400).json({ success: false , message:"Invalid signature"});
     }
   } catch (error) {
-    console.error ("Verify Error:", error)
+    console.error ("Verify Error:", error);
     res.status(500).json({success: false});
   }
 });

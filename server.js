e7156a142de91ff2cf0fd1204d5349b9ebe50f7c
Annu <annuyadav0700@gmail.com>
2026-02-15 12:21:48 +0530

    added static uploads folder

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const fs = require("fs");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/uploads",
+  express.static("uploads")
+);
 
 /* ================= UPLOAD FOLDER ================= */
 if (!fs.existsSync("uploads")) fs.mkdirSync("uploads");

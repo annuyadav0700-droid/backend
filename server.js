7f88de287979c1d90c0a96b61a85ffc005fd9daf
Annu <annuyadav0700@gmail.com>
2026-02-15 12:14:07 +0530

    fixed upload filename

--- server.js
+++ server.js
@@ -20,14 +20,21 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.post("/upload", upload.array("files"), (req, res) => {
-  console.log("FILES RECEIVED:", req.files);
-  if (!req.files|| req.files.length === 0) {
-    return res.json ({success: false});
-  }
-  const uploadedFileName = req.files[0].filename;
-  res.json({ 
-    success: true, 
-    filesName: uploadedFileName });
+  try{
+    if (!req.files||req.files.length === 0) {
+      return res.json({success:false});
+    }
+  
+  console.log("Uploaded file:", req.files[0].filename);
+  res.json({
+    success:true,
+    filename:req.files[0].filename
+  });
+} catch (err) {
+  console.error("Upload error:",err);
+  res.json({success: false});
+}
+  
 });
 
 /* ================= RAZORPAY ================= */
